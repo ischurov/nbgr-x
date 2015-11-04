1b8237f71b9b230329bebad23ad9a025d452ec30
ischurov <ilya@schurov.com>
2015-11-04 17:26:31 +0300

    problemset model added

--- nbgr_x.py
+++ nbgr_x.py
@@ -1,23 +1,32 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, url_for, request, abort, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.security import Security, SQLAlchemyUserDatastore, \
-    UserMixin, RoleMixin, login_required
+    UserMixin, RoleMixin, login_required, current_user
 
+from flask_security.utils import encrypt_password
 from flask_mail import Mail
+from flask_admin.contrib import sqla
+from flask_admin import helpers as admin_helpers
+
+import flask_admin
 
 from flask_security.forms import RegisterForm
-from wtforms import Form, BooleanField, TextField, PasswordField, validators
+from wtforms import Form, BooleanField, TextField, PasswordField, SelectMultipleField, validators
+from wtforms.ext.sqlalchemy.fields import QuerySelectMultipleField
 
 # Create app
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-        'sqlite:////srv/data/nbgr-x/2015-16/students.sqlite'
+app.config.from_pyfile('config.py')
+#app.config['SECRET_KEY'] = 'secret'
+#app.config['SQLALCHEMY_DATABASE_URI'] = \
+#        'sqlite:////srv/data/nbgr-x/2015-16/students.sqlite'
 app.config['DEBUG'] = True
 
 # 'SECURITY_CONFIRMABLE',
-for field in ['SECURITY_REGISTERABLE','SECURITY_RECOVERABLE', 'SECURITY_LOGIN_WITHOUT_CONFIRMATION']:
-    app.config[field] = True
+#for field in ['SECURITY_REGISTERABLE','SECURITY_RECOVERABLE', 'SECURITY_LOGIN_WITHOUT_CONFIRMATION']:
+#    app.config[field] = True
 
 # After 'Create app'
 # app.config['MAIL_TESTING'] = True
@@ -37,39 +46,124 @@ roles_users = db.Table('roles_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
         db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
+courses_users = db.Table('courses_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('course_id', db.Integer(), db.ForeignKey('course.id')))
+
+
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
+    def __str__(self):
+        return self.name
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean())
+    year_begin = db.Column(db.Integer)
+    name = db.Column(db.String(255), unique=True)
+    description = db.Column(db.String(255))
+    problemsets = db.relationship('ProblemSet', backref='course',
+            lazy='dynamic')
+
+
+    def __init__(self, name="", year_begin=1984, active = True, description = ""):
+        self.name = name 
+        self.year_begin = year_begin
+        self.active = active 
+        self.description = description
+
+    def __str__(self):
+        return self.name
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True, index=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
-    firstname = db.Column(db.String(255))
-    lastname = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
     
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
+    courses = db.relationship('Course', secondary=courses_users,
+                             backref=db.backref('users',lazy='dynamic'))
+            
+    def __str__(self):
+        return self.email
+
+class ProblemSet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean())
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    ipynb = db.Column(db.String(255))
+    deadline = db.Column(db.DateTime)
+    name = db.Column(db.String(255), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+
 
 # Setup Flask-Security
 class ExtendedRegisterForm(RegisterForm):
-    firstname = TextField('First Name', [validators.Required()])
-    lastname = TextField('Last Name', [validators.Required()])
+    first_name = TextField('First Name', [validators.Required()])
+    last_name = TextField('Last Name', [validators.Required()])
+#    active_courses = Course.query.filter_by(active = True).all()
+#    courses_choices = [(c.id, c.description) for c in active_courses]
+    courses = QuerySelectMultipleField(
+            query_factory = lambda: Course.query.filter_by(active=True),
+            get_label='description')
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore,
          confirm_register_form=ExtendedRegisterForm,
          register_form=ExtendedRegisterForm)
 
+class MyModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        if not current_user.is_active() or not current_user.is_authenticated():
+            return False
+
+        if current_user.has_role('superuser'):
+            return True
+
+        return False
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated():
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
+
 # Create a user to test with
-@app.before_first_request
-def create_user():
+# @app.before_first_request
+def build_sample_db():
     db.create_all()
-    user_datastore.create_user(email='user@example.com', password='password')
-    db.session.commit()
+    with app.app_context():
+        user_role = Role(name='user')
+        super_user_role = Role(name='superuser')
+        db.session.add(user_role)
+        db.session.add(super_user_role)
+        db.session.commit()
+
+        user_datastore.create_user(email='admin', password=encrypt_password('admin'),
+        first_name='Admin', roles=[user_role, super_user_role])
+    
+
+        db.session.commit()
 
 # Views
 @app.route('/')
@@ -77,5 +171,33 @@ def create_user():
 def home():
     return render_template('index.html')
 
+# Create admin
+admin = flask_admin.Admin(
+    app,
+    'nbgr-x',
+    base_template='my_master.html',
+    template_mode='bootstrap3',
+)
+
+# Add model views
+admin.add_view(MyModelView(Role, db.session))
+admin.add_view(MyModelView(User, db.session))
+admin.add_view(MyModelView(Course, db.session))
+admin.add_view(MyModelView(ProblemSet, db.session))
+
+# define a context processor for merging flask-admin's template context into the
+# flask-security views.
+@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template=admin.base_template,
+        admin_view=admin.index_view,
+        h=admin_helpers,
+    )
+
 if __name__ == '__main__':
+    app_dir = os.path.realpath(os.path.dirname(__file__))
+    database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
+    if not os.path.exists(database_path):
+        build_sample_db()
     app.run()
