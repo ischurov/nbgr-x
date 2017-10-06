b087cf5464bb749624bf503d534135d5d9c931c8
Ilya V. Schurov <ilya@schurov.com>
2017-10-06 14:22:41 +0300

    student name is in feedback

--- nbgr_x.py
+++ nbgr_x.py
@@ -1060,8 +1060,8 @@ def get_feedback(id):
         lines = f.readlines()
     for i, line in enumerate(lines):
         if 'Score' in line:
-            lines[i] = re.sub(r'Score: (\d+\.\d+)',
-                       r'\0 \|' + re.escape(submission.user.first_name) +
+            lines[i] = re.sub(r'(Score: (\d+\.\d+))',
+                       r'\1 | ' + re.escape(submission.user.first_name) +
                        ' ' + re.escape(submission.user.last_name), line)
             break
 
